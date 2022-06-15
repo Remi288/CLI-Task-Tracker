aa7494fc0a340603ba1b332e59a6b4a7567ecc40
remi288 <remizcode200@yahoo.com>
2022-06-15 23:43:06 +0100

    Update the todocli

--- todocli.py
+++ todocli.py
@@ -3,6 +3,8 @@ from rich.console import Console
 from rich.table import Table
 
 from helper import get_category_color
+from model import Todo
+from database import add_todo, get_all_todos, delete_todo, complete_todo, update_todo
 
 
 console = Console()
@@ -13,30 +15,35 @@ app = typer.Typer()
 def add(task: str, category:str):
     """Adds an item to the todo list"""
     typer.echo(f'Adding {task} to {category}')
+    todo = Todo(task, category)
+    add_todo(todo)
     show()
 
 @app.command(short_help='removes an item from the todo list')
 def delete(postion:int):
     """Removes an item from the todo list"""
     typer.echo(f'Deleting item {postion}')
+    delete_todo(postion-1)
     show()
 
 @app.command(short_help='update items in the todo list')
 def update(postion:int, task:str = None, category:str = None):
     """Updates an item in the todo list"""
     typer.echo(f'Updating item {postion}')
+    update_todo(postion-1, task, category)
     show()
 
 @app.command(short_help='complete an item in the todo list')
 def complete(postion:int):
     """Completes an item in the todo list"""
     typer.echo(f'Completing item {postion}')
+    complete_todo(postion-1)
     show()
 
 @app.command(short_help='list all items in the todo list')
 def show():
     """Lists all items in the todo list"""
-    tasks = [('Task 1', 'Sports'), ('Task 2', 'Tutorial'), ('Task 3', 'Youtube')]
+    tasks = get_all_todos()
     console.print("[bold magenta]Todos!:[/bold magenta]", "💻")
 
     table = Table(show_header=True, header_style="bold blue")
@@ -45,9 +52,9 @@ def show():
     table.add_column("Category", min_width=12, justify="right")
     table.add_column("Status", min_width=12, justify="right")
     for i, task in enumerate(tasks, start=1):
-        color = get_category_color(task[1])
-        is_done_str = "✅" if True == 2 else "❌"
-        table.add_row(str(i), task[0], f'[{color}]{task[1]}[/{color}]', is_done_str)
+        color = get_category_color(task.category)
+        is_done_str = "✅" if task.status == 2 else "❌"
+        table.add_row(str(i), task.task, f'[{color}]{task.category}[/{color}]', is_done_str)
     console.print(table)
 
 
